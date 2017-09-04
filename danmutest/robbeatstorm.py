a166e005b8d5acf471a45af1f0d780679f4204c8
xiaohe <2754694716@qq.com>
2017-09-04 16:29:57 +0800

    thread beat storm

--- danmutest/robbeatstorm.py
+++ danmutest/robbeatstorm.py
@@ -1,8 +1,30 @@
 # -*- coding: UTF-8 -*-
-import random, time, urllib, threading, json 
+import threading, re, requests, time
 import robgift
-import bilibilicookie
 
+from danmu import DanMuClient
+
+"""
+Other message received
+local: 2017 09 04 15:25:23
+{'cmd': 'SYS_GIFT', 'msg': '阿梓家的猛男:? 在直播间 :?80397:? 使用了 20 倍节奏风暴，大家快去跟风领取奖励吧！',
+ 'tips': '【阿梓家的猛男】在直播间【80397】使用了 20 倍节奏风暴，大家快去跟风领取奖励吧！', 
+ 'giftId': 39, 'msgTips': 1, 'url': 'http://live.bilibili.com/80397', 
+ 'roomid': 80397, 'rnd': 1504509162, 'NickName': '', 'Content': '', 'MsgType': 'other'}
+ 
+ 
+ 
+
+{"cmd": "SPECIAL_GIFT", "data": {"39": {"id": "114259", "num": 100, "time": 90,
+"content": "刚刚回帝都了，庆祝下", "hadJoin": 0, "action": "start"}}, "NickName"
+: "", "Content": "", "MsgType": "other"}
+Other message received
+local: 2017 08 30 15:15:22
+{"cmd": "SPECIAL_GIFT", "data": {"39": {"action": "end"}}, "NickName": "", "Cont
+ent": "", "MsgType": "other"}
+ 
+Other message end
+"""
 
 def isBeatstorm(msg):
     try:
@@ -14,31 +36,84 @@ def isBeatstorm(msg):
     except:
         pass
     return False
-
-
-def robBeatstorm(msg):
-#url = ('http://live.bilibili.com/'
-#+ self.url.split('/')[-1] or self.url.split('/')[-2])
-#self.roomId = re.findall(b'var ROOMID = (\d+);', requests.get(url).content)[0].decode('ascii')
-    print('rob beat storm')
+def isGlobalBeatstrom(msg):
     try:
+        if (msg['cmd'] == 'SYS_GIFT') and msg['giftId'] == 39:
+            msg['url']
+            return True  
+    except:
+        pass
+    return False   
+
+def robBeatstorm(msg, realroomid = 356767):
+   
+    print('rob local beat storm')
+    try:
+        realroomid = int(realroomid)
         x =  msg['data']['39']
         content = x['content']
     except:
         print ("rob beatstorm error")
         return
 
-    robgift.sendDanmu(356767, content)
+    robgift.sendDanmu(realroomid, content)
     
     return
 
-
+def robglobalstormwork(msg):
+    url = msg['url']
+    dmc = DanMuClient(url)
+    if not dmc.isValid(): print('Url not valid')
+    
+    realroomId = re.findall(b'var ROOMID = (\d+);', requests.get(url).content)[0].decode('ascii')
+    
+    @dmc.other
+    def other_fn(msg, realroomid = realroomId):
+        if not isBeatstorm(msg) : return
+        
+        robBeatstorm(msg, realroomid)
+        time.sleep(2.0)
+        robBeatstorm(msg, realroomid)
+        time.sleep(2.0)
+        robBeatstorm(msg, realroomid)
+        time.sleep(2.0)
+        robBeatstorm(msg, realroomid)
+        time.sleep(2.0)
+        robBeatstorm(msg, realroomid)
+        
+        print('rob global beat storm  end')
+        
+        pass
+    
+    dmc.start(blockThread=False)
+    time.sleep(100)
+    dmc.stop()
+    
+def robglobaltest(msg):
+    try:
+        robglobalstormwork(msg)
+    except Exception as e:
+        print('robglobaltest',e)
+     
+def robGlobalstorm(msg):
+    print('rob global beat storm begin')
+    t = threading.Thread(target = robglobaltest, args = (msg,));
+    t.setDaemon(True)
+    t.start()
+    
+    pass
 
 def robBeatstormWork(msg):
     try:
-        if not isBeatstorm(msg): return
-        robBeatstorm(msg)
+        if isBeatstorm(msg):
+            robBeatstorm(msg)
+            return
     except Exception as e:
-        print ('rob beat storm fail', e)
+        print ('rob local beat storm fail', e)
     
-    
+    try:
+        if (isGlobalBeatstrom(msg)):
+            robGlobalstorm(msg)
+            return
+    except Exception as e:
+        print ('rob glabol beat storm fail', e)
